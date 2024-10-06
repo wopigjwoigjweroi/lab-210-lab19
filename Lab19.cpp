a7281b8311daaf83bee5f34dbcc050efa27d884b
wopigjwoigjweroi <agizekel@gmail.com>
2024-10-06 12:20:46 -0700

    Commit 2

--- Lab19.cpp
+++ Lab19.cpp
@@ -1,6 +1,8 @@
+// COMSC-210 | Lab 19 | Jeremiah Ortiz
 #include <iostream>
 #include <string>
 #include <fstream> 
+#include <vector> 
 using namespace std; 
 
 struct Node {
@@ -12,3 +14,27 @@ struct Node {
     string comment; 
 
 };
+
+class Movie {
+
+        private:
+            string title; 
+
+            Node *head; 
+
+        public: 
+
+            Movie(string title) : title(title), head(nullptr) {}
+
+            ~Movie() {
+                while (head) {
+                    Node *temp = head; 
+
+                    head = head->next; 
+
+                    delete temp; 
+                }
+            }
+
+            
+}; 
